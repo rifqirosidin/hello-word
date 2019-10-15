e53ee69db68c2eefdc1ce21d1683c8e2a0becf50
Rifqi Rosidin <rifqi.godog@gmail.com>
2019-10-15 11:39:07 +0700

    add skor a +point 3

--- app/src/main/java/com/dts/helloworld/MainActivity.java
+++ app/src/main/java/com/dts/helloworld/MainActivity.java
@@ -3,12 +3,28 @@ package com.dts.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int skorA = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lain);
+    }
+
+    public void tambah3PoinA(View view) {
+        skorA += 3;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText(String.valueOf(skorA));
+    }
+
+    public void tambah2PoinA(View view) {
+    }
+
+    public void tambah1PoinA(View view) {
+
     }
 }
